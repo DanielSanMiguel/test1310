0ac8093bc4a33f94a97a8ff96df3013678c50652
DanielSanMiguel <146175551+DanielSanMiguel@users.noreply.github.com>
2023-10-13 13:48:48 +0200

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -10,10 +10,5 @@ from selenium import webdriver
 
 # Inicializa el navegador web
 driver = webdriver.Chrome()  # Cambia a webdriver.Firefox() si estás usando Firefox
-
-# Define la función para tu aplicación
-
 st.title("Automatizando un navegador con Selenium en Streamlit")
-
-    # Puedes usar Selenium para realizar acciones en el navegador web
-driver.get("https://www.ejemplo.com")  # Reemplaza con tu URL
+driver.get("https://drones.enaire.es/")
