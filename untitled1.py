28bad3f45d4299a41868db733f3fa98923146480
DanielSanMiguel <146175551+DanielSanMiguel@users.noreply.github.com>
2023-10-13 18:55:21 +0200

    Add files via upload

--- untitled1.py
+++ untitled1.py
@@ -6,13 +6,9 @@ Created on Fri Oct 13 13:34:43 2023
 """
 import streamlit as st
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 
-# Create a new Chrome driver instance
-driver = webdriver.Chrome(ChromeDriverManager().install())
-
-# Use the driver to interact with web pages
-driver.get('https://drones.enaire.es/')
+options = webdriver.ChromeOptions()
+st.write(options.to_capabilities())
 
 
 
