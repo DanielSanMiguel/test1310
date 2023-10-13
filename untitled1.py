1f34e4f6ae26b3dd94089b6b5a654af350845484
DanielSanMiguel <146175551+DanielSanMiguel@users.noreply.github.com>
2023-10-13 19:16:02 +0200

    Add files via upload

--- untitled1.py
+++ untitled1.py
@@ -5,12 +5,26 @@ Created on Fri Oct 13 13:34:43 2023
 @author: dsanm
 """
 import streamlit as st
+
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-options = webdriver.ChromeOptions()
-st.write(options.to_capabilities())
+@st.experimental_memo
+def get_driver():
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+options = Options()
+options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+
+driver = get_driver()
+driver.get('https://drones.enaire.es/')
+
+st.code(driver.page_source)
 
 
-
+# Use the driver to interact with web pages
 
 
