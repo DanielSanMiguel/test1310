da70ba9e2004dee766332034b2b2c207c85173bc
DanielSanMiguel <146175551+DanielSanMiguel@users.noreply.github.com>
2023-10-13 20:38:13 +0200

    Add files via upload

--- untitled1.py
+++ untitled1.py
@@ -1,12 +1,67 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Oct 13 13:34:43 2023
+Created on Fri Oct 13 20:33:58 2023
 
 @author: dsanm
 """
+
+import os
+
 import streamlit as st
-from seleniumbase import DriverContext
-from seleniumbase import BaseCase
 from selenium import webdriver
-with DriverContext(browser="chrome", incognito=True) as driver:
-    driver.open("drones.enaire.es/")
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+
+options = Options()
+
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-features=NetworkService")
+options.add_argument("--window-size=1920x1080")
+options.add_argument("--disable-features=VizDisplayCompositor")
+
+
+def delete_selenium_log():
+    if os.path.exists('selenium.log'):
+        os.remove('selenium.log')
+
+
+def show_selenium_log():
+    if os.path.exists('selenium.log'):
+        with open('selenium.log') as f:
+            content = f.read()
+            st.code(content)
+
+
+def run_selenium():
+
+    with webdriver.Chrome(options=options, service_log_path='selenium.log') as driver:
+        url = "'https://drones.enaire.es/'"
+        driver.get(url)
+
+
+
+if __name__ == "__main__":
+    delete_selenium_log()
+    st.set_page_config(page_title="Selenium Test", page_icon='✅',
+        initial_sidebar_state='collapsed')
+    st.title('🔨 Selenium Test for Streamlit Sharing')
+    st.markdown('''This app is only a very simple test for **Selenium** running on **Streamlit Sharing** runtime.<br>
+        The suggestion for this demo app came from a post on the Streamlit Community Forum.<br>
+        <https://discuss.streamlit.io/t/issue-with-selenium-on-a-streamlit-app/11563><br>
+        This is just a very very simple example and more a proof of concept.
+        A link is called and waited for the existence of a specific class and read it.
+        If there is no error message, the action was successful.
+        Afterwards the log file of chromium is read and displayed.
+        ---
+        ''', unsafe_allow_html=True)
+
+    st.balloons()
+    if st.button('Start Selenium run'):
+        st.info('Selenium is running, please wait...')
+        result = run_selenium()
+        st.info(f'Result -> {result}')
+        st.info('Successful finished. Selenium log file is shown below...')
+        show_selenium_log()
