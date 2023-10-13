81af5d647871fdc72187550a35ca4d86a5b36b4d
DanielSanMiguel <146175551+DanielSanMiguel@users.noreply.github.com>
2023-10-13 18:46:25 +0200

    Add files via upload

--- untitled1.py
+++ untitled1.py
@@ -6,17 +6,15 @@ Created on Fri Oct 13 13:34:43 2023
 """
 import streamlit as st
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import chromedriver_binary
-
-# Set up Chrome options
-chrome_options = Options()
-#chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+from webdriver_manager.chrome import ChromeDriverManager
 
 # Create a new Chrome driver instance
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 # Use the driver to interact with web pages
 driver.get('https://drones.enaire.es/')
+
+
+
 
 
