68ae444d1fbbc05f2d83aea94d67a47d34f95562
DanielSanMiguel <146175551+DanielSanMiguel@users.noreply.github.com>
2023-10-13 20:56:02 +0200

    Add files via upload

--- untitled1.py
+++ untitled1.py
@@ -1,59 +1,30 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct 13 20:33:58 2023
-
-@author: dsanm
-"""
-
-import os
-
 import streamlit as st
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
 
-options = Options()
+"""
+## Web scraping on Streamlit Cloud with Selenium
 
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-gpu")
-options.add_argument("--disable-features=NetworkService")
-options.add_argument("--window-size=1920x1080")
-options.add_argument("--disable-features=VizDisplayCompositor")
+[![Source](https://img.shields.io/badge/View-Source-<COLOR>.svg)](https://github.com/snehankekre/streamlit-selenium-chrome/)
 
+This is a minimal, reproducible example of how to scrape the web with Selenium and Chrome on Streamlit's Community Cloud.
 
-def delete_selenium_log():
-    if os.path.exists('selenium.log'):
-        os.remove('selenium.log')
+Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
+"""
 
+with st.echo():
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
 
-def show_selenium_log():
-    if os.path.exists('selenium.log'):
-        with open('selenium.log') as f:
-            content = f.read()
-            st.code(content)
+    @st.experimental_singleton
+    def get_driver():
+        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
+    options = Options()
+    options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
 
-def run_selenium():
+    driver = get_driver()
+    driver.get("http://example.com")
 
-    with webdriver.Chrome(options=options, service_log_path='selenium.log') as driver:
-        url = "'https://drones.enaire.es/'"
-        driver.get(url)
-
-
-
-if __name__ == "__main__":
-    delete_selenium_log()
-    st.set_page_config(page_title="Selenium Test", page_icon='✅',
-        initial_sidebar_state='collapsed')
-    st.title('🔨 Selenium Test for Streamlit Sharing')
-
-
-    st.balloons()
-    if st.button('Start Selenium run'):
-        st.info('Selenium is running, please wait...')
-        result = run_selenium()
-        st.info(f'Result -> {result}')
-        st.info('Successful finished. Selenium log file is shown below...')
-        show_selenium_log()
+    st.code(driver.page_source)
