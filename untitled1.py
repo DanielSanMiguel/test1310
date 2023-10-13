b151705f227c9eeb26b5d60238fb8ddd0bc0da00
DanielSanMiguel <146175551+DanielSanMiguel@users.noreply.github.com>
2023-10-13 19:47:11 +0200

    Add files via upload

--- untitled1.py
+++ untitled1.py
@@ -5,18 +5,14 @@ Created on Fri Oct 13 13:34:43 2023
 @author: dsanm
 """
 import streamlit as st
-
+from seleniumbase import Driver
 from seleniumbase import BaseCase
 from selenium import webdriver
 
-class MyTest(BaseCase):
-    def test_open_browser(self):
-        self.driver = webdriver.Chrome()
-        self.driver.get("https://drones.enaire.es/")
 
+driver = Driver(browser="chrome", headless=False)
 
+driver.get("https://drones.enaire.es/")
 
-if __name__ == "__main__":
-    MyTest().main()
 
 
